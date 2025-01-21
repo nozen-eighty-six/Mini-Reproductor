dec558490de8561255b38a61555b0d233d3198f7
yuusha <yuushaescalante@gmail.com>
2025-01-21 07:22:52 -0500

    setting the type of expected props.

--- src/components/Home/ArtistPlayer.jsx
+++ src/components/Home/ArtistPlayer.jsx
@@ -1,4 +1,5 @@
-const ArtistPlayer = () => {
+import PropTypes from "prop-types";
+const ArtistPlayer = ({ songTitle, artistName }) => {
   console.log("ArtistPlayer ");
   return (
     <div className="artist__container flex  gap-4">
@@ -9,14 +10,19 @@ const ArtistPlayer = () => {
       />
       <div className="artist__info flex flex-col">
         <span className="song__name text-white text-base font-bold">
-          Tu última canción
+          {songTitle || ""}
         </span>
         <span className="artist__name text-white/70 text-sm">
-          Los Temerarios
+          {artistName || ""}
         </span>
       </div>
     </div>
   );
+};
+
+ArtistPlayer.propTypes = {
+  songTitle: PropTypes.string,
+  artistName: PropTypes.string,
 };
 
 export default ArtistPlayer;
