40f79b29ed34511147dbd8e673788d5d0107a3da
yuusha <yuushaescalante@gmail.com>
2025-01-21 07:57:32 -0500

    Creating the Music Player Footer component to navigate, search and display user information.

--- src/components/PlayerNav.jsx
+++ src/components/PlayerNav.jsx
@@ -1,13 +1,17 @@
+import { NavLink } from "react-router";
 import MusicPlayerFooter from "./Home/MusicPlayerFooter";
-import SwiperSlider from "./Home/SwiperSlider";
+import { useSelector } from "react-redux";
 
 const PlayerNav = () => {
+  const { user } = useSelector((state) => state.user);
+  console.log("PlayerNav");
+  console.log(user);
   return (
     <>
-      <header className="header py-5    w-full h-full  ">
+      <header className="header     w-full h-full  ">
         <nav className="nav  h-full   rounded-t-2xl rounded-b-2xl bg-[#181b22] flex flex-col py-4 px-3 text-xl ">
           <a
-            href=""
+            href="/"
             className="nav__logo inline-flex justify-center items-center mb-16 text-white gap-y-5 gap-x-2  "
           >
             <img src="/Images/azur-lane-logo.png" alt="" className="w-[15%]" />
@@ -17,32 +21,32 @@ const PlayerNav = () => {
           <div className="nav__menu ">
             <ul className="nav__list flex flex-col gap-2 text-white ">
               <li className="nav__item">
-                <a href="" className="nav__link">
+                <NavLink to={"/"} className="nav__link   w-full h-full">
                   <i className="ri-home-line"></i>
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav__item">
-                <a href="" className="nav__link">
+                <NavLink to={"/categories"} className="nav__link w-full h-full">
                   <i className="ri-list-unordered"></i>
                   Categories
-                </a>
+                </NavLink>
               </li>
               <li className="nav__item">
-                <a href="" className="nav__link">
+                <NavLink to={"/playlist"} className="nav__link w-full h-full">
                   <i className="ri-play-list-line"></i>
                   Playlist
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
         </nav>
       </header>
       <div className="nav__options  ">
-        <div className="pt-5  flex justify-between">
+        <div className="h-full  flex justify-between ">
           <form
             action=""
-            className="nav__search flex items-center  w-[480px] py-[10px] px-5 bg-[#23262d] rounded-3xl"
+            className="nav__search flex items-center h-[45px] w-[480px] py-[10px] px-5 bg-[#23262d] rounded-3xl"
           >
             <input
               type="text"
@@ -63,16 +67,18 @@ const PlayerNav = () => {
             </div>
             <div className="user__info flex flex-col justify-center gap-1">
               <span className="user__name text-white text-sm font-bold">
-                Edgar Escalante
+                {user.name || ""}
               </span>
               <span className="user__type text-white/75 bg-[#082b33] text-sm rounded-full w-max px-[20px] py-0">
-                User
+                {Object.keys(user).length === 0
+                  ? ""
+                  : user.roles[0]?.name.charAt(0).toUpperCase() +
+                    user.roles[0]?.name.substring(1).toLowerCase}
               </span>
             </div>
           </div>
         </div>
       </div>
-      <SwiperSlider />
       <MusicPlayerFooter />
     </>
   );
