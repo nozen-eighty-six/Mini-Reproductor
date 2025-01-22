0d56092171577f8622c1583ed95bbe58b0c7b92c
yuusha <yuushaescalante@gmail.com>
2025-01-21 22:57:07 -0500

    Import the stylesheet to use RemixIcon.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import "./index.css";
+import "remixicon/fonts/remixicon.css";
+
 import App from "./App.jsx";
 import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
